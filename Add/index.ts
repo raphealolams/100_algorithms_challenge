b2f950eb5f83c341b1182f5d778615c75c4a891b
raphealolams <raphealolams@gmail.com>
2019-02-21 09:25:27 +0100

    from last night

---
+++ Add/index.ts
@@ -0,0 +1,21 @@
+function add(params1: number, params2: number): number {
+     return params1 + params2
+}
+
+
+function add2 (...params1: number[]) : number{
+    let total = 0
+
+
+
+    params1.forEach((num) => {
+        total += num
+    })
+
+    return total
+}
+
+
+console.log(add(2, 6))
+console.log(add2(1,2,3,4,5,0))
+
